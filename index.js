923f72228bccb98cb9bea02b9be5dcebdc9559b7
fadumasaidcodes <fadumasaid2022@gmail.com>
2024-05-05 20:32:19 +0100

    commit message

---
+++ index.js
@@ -0,0 +1,11 @@
+//document.getElementById('count-el').innerText = 5; 
+
+// somewhere to store the data we need variables to store the data
+
+// let count be zero
+// Somewhere to store the data: variables to store the data
+
+// Let count be zero
+let count = 0;
+
+console.log(count);
